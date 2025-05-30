aaf0bb6630fe6ffd0e4328e7138982a4da39581e
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2025-05-30 12:29:07 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,15 +1,15 @@
 import os
 
 
-API_ID: int = int(os.getenv("API_ID", 0))
+API_ID: int = int(os.getenv("API_ID", '3898418'))
 
-API_HASH: str = os.getenv("API_HASH", "")
+API_HASH: str = os.getenv("API_HASH", "5a82313211da04d63297bd4de436228c")
 
 SESSION_STRING: str = os.getenv("SESSION_STRING", "")
 
-OWNER_ID = [int(id.strip()) for id in os.getenv("OWNER_ID", "0").split(",")]
+OWNER_ID = [int(id.strip()) for id in os.getenv("OWNER_ID", "7822547078").split(",")]
 
-LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", 0))
+LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", '-1001747680884'))
 
 PREFIX: str = str(".")
 
